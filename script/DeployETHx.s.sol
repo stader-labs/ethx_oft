f78442c8d0fa2e3f854452ff63a1b57a7d318f4a
John Cairns <john@2ad.com>
2024-04-23 19:49:26 -0500

    initial deployment for ETHx and OFT

---
+++ script/DeployETHx.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GNU-3.0-or-later
+pragma solidity ^0.8.22;
+
+import { Script, console } from "forge-std/Script.sol";
+
+import { TransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import { ETHx } from "../contracts/ETHx.sol";
+
+contract DeployETHx is Script {
+    event DeployedETHx(address ethxProxy, address ethx);
+
+    function deployProxy() public {
+        address admin = vm.envAddress("ETHX_ADMIN");
+        vm.startBroadcast();
+        ETHx implementation = new ETHx();
+        bytes memory initializationData = abi.encodeWithSelector(ETHx.initialize.selector, admin);
+        TransparentUpgradeableProxy proxy =
+            new TransparentUpgradeableProxy(address(implementation), admin, initializationData);
+        console.log("ETHx deployed to proxy at: ", address(proxy));
+        emit DeployedETHx(address(proxy), address(implementation));
+        vm.stopBroadcast();
+    }
+
+    function setupGrants() public {
+        address proxy = vm.envAddress("ETHX");
+        address admin = vm.envAddress("ETHX_ADMIN");
+        ETHx ethx = ETHx(proxy);
+        ethx.grantRole(ethx.MINTER_ROLE(), admin);
+        ethx.grantRole(ethx.BURNER_ROLE(), admin);
+        ethx.grantRole(ethx.PAUSER_ROLE(), admin);
+    }
+}
--- script/OFT.s.sol
+++ script/OFT.s.sol
@@ -3,12 +3,29 @@ pragma solidity ^0.8.22;
 
 import { Script, console } from "forge-std/Script.sol";
 
+import { ETHx_OFT } from "../contracts/ETHx_OFT.sol";
+import { ETHx } from "../contracts/ETHx.sol";
+
 contract OFT is Script {
     event DeployOFT(address oft);
 
     function deploy() public {
+        address ethx = vm.envAddress("ETHX");
+        address lzEndpoint = vm.envAddress("LZ_ENDPOINT");
+        address delegate = vm.envAddress("DELEGATE");
+
         vm.startBroadcast();
-        console.log("OFT deployed at: ", address(0x0));
+        ETHx_OFT oft = new ETHx_OFT(ethx, lzEndpoint, delegate);
+        console.log("ETHx_OFT deployed to: ", address(oft));
+        setupGrants(ethx, address(oft));
+        emit DeployOFT(address(oft));
         vm.stopBroadcast();
+    }
+
+    function setupGrants(address proxy, address admin) private {
+        ETHx ethx = ETHx(proxy);
+        ethx.grantRole(ethx.MINTER_ROLE(), admin);
+        ethx.grantRole(ethx.BURNER_ROLE(), admin);
+        ethx.grantRole(ethx.PAUSER_ROLE(), admin);
     }
 }
